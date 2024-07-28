c094b0324aa712a38f031e65e41f9014188e9a86
andrelinces <andrelinces@yahoo.com.br>
2024-07-28 09:05:48 -0300

    feat:                                ─╯ Studying Build Apple WatchOS Companion.

--- Movie_ChallengeUITests/Movie_ChallengeUITests.swift
+++ Movie_ChallengeUITests/Movie_ChallengeUITests.swift
@@ -11,7 +11,7 @@ final class Movie_ChallengeUITests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        // remove
+        // remove, testing....
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
 
