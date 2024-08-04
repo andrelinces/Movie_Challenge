243143de2587a0a0f06bd80989bd2c4a61028188
andrelinces <andrelinces@yahoo.com.br>
2024-08-04 18:58:28 -0300

    feat: Studying and implementing the use of the Coordinator Pattern.

--- Movie_ChallengeUITests/Movie_ChallengeUITests.swift
+++ Movie_ChallengeUITests/Movie_ChallengeUITests.swift
@@ -11,7 +11,7 @@ final class Movie_ChallengeUITests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        // Remove comments...refactor code!
+        // Studying and implementing the use of the Coordinator Pattern.
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
 
